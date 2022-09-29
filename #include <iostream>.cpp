1e4fbfb21e5c3e53d2bffb28fc9f5a2c57ff92c9
kazmure <kazzmurenko4002@gmail.com>
2022-09-29 02:11:18 -0400

    init

--- #include <iostream>.cpp
+++ #include <iostream>.cpp
@@ -1,9 +1,39 @@
-#include <iostream>
-using 
+#include <iostream.h>
+#include <iomanip>
+#include <math.h>
+using  namespace std;
+
 
 int main() {
 
-   signed int screen = int::LONG_MAX + 1;
+  setlocale(LC_ALL,"")
+  double a=0 , b=0 , c=0;
+  
+//  ax^2+bx
 
-   cout << screen << endl;
-   return 0; 
+  cout<<"Pls name a:" << endl;
+  cin >> a;
+  cout<<"Pls name b:" << endl;
+  cin >> b;
+  cout<<"Pls name c:" << endl;
+  cin >> c;
+
+  double diskr= pow(b, 2) - 4 * a * c;
+
+  if (sqrt(sdiskr)<0)
+  {
+   cout<< "Nicht" <<endl;
+  }
+
+ else {
+     double sdiskr = sqrt(diskr)
+
+   double x1=(-b - sdiskr)/ 2*a, 
+   x2= (-b - sdiskr)/ 2*a;
+
+   cout << "1" << x1 << x1 << "/n" <<endl;
+   cout << "Pls" << endl;
+ }
+  return 0;
+  
+  }
